e8964ee224d23af0117fa52001d2cf5a9102e929
wirdaini <wirda24si@mahasiswa.pcr.ac.id>
2025-11-15 11:57:21 +0700

    seeder user, warga

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Guest',
+            'email' => 'quin@ub.ac.id',
+            'password' => Hash::make('quinub')
+        ]);
+    }
+}
---
+++ database/seeders/CreateWargaDummy.php
@@ -0,0 +1,46 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CreateWargaDummy extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $faker = Factory::create('id_ID'); // Faker Bahasa Indonesia
+
+        // Data agama dalam Bahasa Indonesia
+        $agamaList = ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu'];
+
+        // Data pekerjaan yang umum di Indonesia
+        $pekerjaanList = [
+            'Wiraswasta', 'PNS', 'Guru', 'Dosen', 'Dokter', 'Perawat',
+            'Karyawan Swasta', 'Petani', 'Nelayan', 'Pedagang', 'Sopir',
+            'Buruh', 'Ibu Rumah Tangga', 'Pelajar/Mahasiswa', 'Pensiunan',
+        ];
+
+        foreach (range(1, 10) as $index) {
+            $jenisKelamin = $faker->randomElement(['L', 'P']);
+            $firstName    = $jenisKelamin === 'L' ? $faker->firstNameMale : $faker->firstNameFemale;
+
+            DB::table('warga')->insert([
+                'user_id'       => User::inRandomOrder()->first()->id,
+                'no_ktp'        => $faker->unique()->numerify('32##############'), 
+                'nama'          => $firstName . ' ' . $faker->lastName,
+                'jenis_kelamin' => $jenisKelamin,
+                'agama'         => $faker->randomElement($agamaList),
+                'pekerjaan'     => $faker->randomElement($pekerjaanList),
+                'telp'          => '+62' . $faker->numerify('8##########'), // +6281234567890 (15 digit)
+                'email'         => $faker->unique()->safeEmail,
+                'created_at'    => now(),
+                'updated_at'    => now(),
+            ]);
+        }
+    }
+}
